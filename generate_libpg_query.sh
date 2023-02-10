6240cbe468bc7ff7ff85996b7494f40f9458b90c
Filipe Roque <froque@premium-minds.com>
2023-02-10 18:14:24 +0000

    Adds so files to be tracked by git
    This allows for easier maven release process, with no need to compile so
    files while running maven-release-plugin

--- generate_libpg_query.sh
+++ generate_libpg_query.sh
@@ -8,18 +8,24 @@ LIBPG_QUERY_TEMP_DIR=$(mktemp --directory)
 
 pushd $LIBPG_QUERY_TEMP_DIR
 
-wget https://github.com/pganalyze/libpg_query/archive/refs/tags/15-4.2.0.zip
-unzip 15-4.2.0.zip
+# compile x86-64
+docker run --name gcc-libpgquery-amd64 \
+  gcc:11 \
+  bash -c 'wget -q https://github.com/pganalyze/libpg_query/archive/refs/tags/15-4.2.0.zip && unzip 15-4.2.0.zip && cd libpg_query-15-4.2.0 && make -j build_shared'
+docker cp gcc-libpgquery-amd64:libpg_query-15-4.2.0/libpg_query.so $PROJECT_DIR/src/main/resources/linux-x86-64/libpg_query.so
 
-docker run --rm \
-  --user $(id -u):$(id -g) \
-  -v "$PWD/libpg_query-15-4.2.0":/usr/src/myapp \
-  -w /usr/src/myapp gcc:11 make build_shared
+# compile aarch64
+docker run --name gcc-libpgquery-aarch64 \
+  gcc:11 \
+  bash -c 'wget -q https://github.com/pganalyze/libpg_query/archive/refs/tags/15-4.2.0.zip && unzip 15-4.2.0.zip && cd libpg_query-15-4.2.0 && apt-get update && apt install -y gcc-aarch64-linux-gnu && export CC=aarch64-linux-gnu-gcc && make -j build_shared'
+docker cp gcc-libpgquery-aarch64:libpg_query-15-4.2.0/libpg_query.so $PROJECT_DIR/src/main/resources/linux-aarch64/libpg_query.so
 
-cp libpg_query-15-4.2.0/libpg_query.so $PROJECT_DIR/src/main/resources/linux-x86-64/libpg_query.so
-
+# generate java sources
 wget https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protoc-21.12-linux-x86_64.zip
 unzip protoc-21.12-linux-x86_64.zip
+
+wget -q https://github.com/pganalyze/libpg_query/archive/refs/tags/15-4.2.0.zip
+unzip 15-4.2.0.zip
 
 rm -rv $PROJECT_DIR/src/main/java/com/premiumminds/sonar/postgres/protobuf/
 
@@ -33,6 +39,7 @@ bin/protoc \
 
 patch -p1 --directory=$PROJECT_DIR < $PROJECT_DIR/Token.java.patch
 
+docker rm -f gcc-libpgquery-amd64 gcc-libpgquery-aarch64
 popd
 chmod -R +w $LIBPG_QUERY_TEMP_DIR
 rm -r $LIBPG_QUERY_TEMP_DIR
